5cfcc4de2171ebe8053ad5128ddf254aa50a92a4
guozhao <davidkoojohn@163.com>
2019-01-25 01:08:00 +0800

    remove jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,8 @@
     </head>
     <body>
         <div id="app"></div>
+        <script src="{{asset('js/manifest.js')}}"></script>
+        <script src="{{asset('js/vendor.js')}}"></script>
         <script src="{{asset('js/app.js')}}" ></script>
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .extract(['react'])
+    .sass('resources/sass/app.scss', 'public/css');
