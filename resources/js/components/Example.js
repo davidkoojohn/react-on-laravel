a37afdc8f9d999898d7e07993f961d60a622773b
guozhao <davidkoojohn@163.com>
2019-01-25 00:08:54 +0800

    use react

---
+++ resources/js/components/Example.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Example extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="card-header">Example Component</div>
+                <div className="card-body">I'm an example component!</div>
+            </div>
+        );
+    }
+}
