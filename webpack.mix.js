521c38c73783821c9e4b49ce795e49c8b5a55307
guozhao <davidkoojohn@163.com>
2019-01-25 09:51:34 +0800

    vendor.js add axios

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
-    .extract(['react'])
+    .extract(['react', 'axios'])
     .sass('resources/sass/app.scss', 'public/css');
